74b33d448887f82dbf88e681efedefed2b061e43
mxk <1005682757@qq.com>
2015-11-14 23:39:17 +0800

    Merge branch 'master' of https://github.com/mengxk/mvc-javascript-web
    # Conflicts:
    #	view/第3章模型和数据/持久化记录.html

--- resource/js/app/util.js
+++ resource/js/app/util.js
@@ -34,10 +34,10 @@ if (typeof Object.create !== "function") {
 	};
 }
 
-Math.guid = function(){
-	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c){
-		var r = Math.random() * 16|0,
-			v = c == 'x' ? r : (r&0x3\0x8);
+Math.guid = function() {
+	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0,
+			v = c == 'x' ? r : (r & 0x3 | 0x8);
 		return v.toString(16);
 	}).toUpperCase();
 };
